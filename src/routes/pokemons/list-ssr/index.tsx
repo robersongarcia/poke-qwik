01148cd3c55b1d1d241dddbd4d14c8f5ef61012f
Roberson Garcia <roberson.gar12@gmail.com>
2023-07-25 23:25:58 -0400

    fetch pokemons list

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,10 +1,42 @@
 import { component$ } from '@builder.io/qwik';
-import { type DocumentHead } from '@builder.io/qwik-city';
+import { type DocumentHead, Link, routeLoader$ } from '@builder.io/qwik-city';
+import { PokemonsInfo, type PokemonListResponse } from "../../../interfaces/pokemon-list-response";
+
+
+export const usePokemonList = routeLoader$<PokemonsInfo[]>(async () => {
+
+  const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`)
+  const data = await resp.json() as PokemonListResponse
+
+  return data.results
+
+})
 
 export default component$(() => {
+
+    const pokemonData = usePokemonList()
+
     return (<>
-                <h1>ListSSR</h1>
-            </>)
+                <div class="flex flex-col">
+                  <span class="my-5 text-5xl">Status</span>
+                  <span>Current Offset: xxxx</span>
+                  <span>Is loading page: xxxx</span>
+                </div>
+
+                <div class="mt-10">
+                  <Link class="btn btn-primary mr-2">Prev</Link>
+                  <Link class="btn btn-primary mr-2">Next</Link>
+                </div>
+
+                <div class="grid grid-cols-6 mt-5">
+                  <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                  {
+                    pokemonData.value.map((pokemon)=> (
+                      <div class="m-5 flex flex-col justify-center items-center" key={pokemon.name}><span class="capitalize">{pokemon.name}</span></div>
+                    ))
+                  }
+                </div>
+            </>)          
 });
 
 
